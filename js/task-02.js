e064979e0f6d36a0b7e8660ead5a512e5513e7ab
Julia-Gavrashenko <julia.gavrashenko@gmail.com>
2022-12-09 20:47:35 +0200

    forth-commit

--- js/task-02.js
+++ js/task-02.js
@@ -1,11 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
+const elements = ingredients.map((element) => {
+  const itemEl = document.createElement(`li`);
+  itemEl.classList.add(`item`);
+  itemEl.textContent = element;
 
+  return itemEl;
+});
 
+console.log(elements);
+
+const mainEl = document.querySelector(`#ingredients`);
+mainEl.append(...elements);
