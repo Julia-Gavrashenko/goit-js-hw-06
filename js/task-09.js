1c5f775411a8ba16132fb8ec0f0ce268a34ca386
Julia-Gavrashenko <julia.gavrashenko@gmail.com>
2022-12-11 19:03:09 +0200

    fifteenth-commit

--- js/task-09.js
+++ js/task-09.js
@@ -7,12 +7,11 @@ const textEl = document.querySelector(`.color`);
 const divEl = document.querySelector(`.widget`);
 const bodyEl = divEl.parentNode;
 
-console.log(textEl.textContent);
 
 buttonEl.addEventListener(`click`, onBtnClick);
 
 function onBtnClick(event) {
-  console.log(event.currentTarget);
-  bodyEl.style.backgroundColor = getRandomHexColor();
-  textEl.textContent = getRandomHexColor();
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  textEl.textContent = randomColor;
 }
